4ec6406d7b31ac6df8368e2d8e1743b7b50ed1d8
gitVhennz <vhennzgumera31@gmail.com>
2024-10-11 10:06:52 +0800

    Add files via upload
     4 sorted arrays

---
+++ InsertionSort.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class InsertionSort {
+
+    public static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+
+        int[] array = new int[n];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        System.out.println("Original array:");
+        printArray(array);
+
+        insertionSort(array);
+
+        System.out.println("Sorted array:");
+        printArray(array);
+
+        scanner.close();
+    }
+}
---
+++ SelectionSort.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class SelectionSort {
+
+    public static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+
+        int[] array = new int[n];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        System.out.println("Original array:");
+        printArray(array);
+
+        selectionSort(array);
+
+        System.out.println("Sorted array:");
+        printArray(array);
+
+        scanner.close();
+    }
+}
